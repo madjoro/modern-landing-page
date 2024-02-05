384754d28d7f0692466be5ac8ee1b7a60921bcf1
madjo <78071957+madjo7@users.noreply.github.com>
2024-02-05 15:34:41 +0100

    Finished GetStarted comp, minor fixes and text changes

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -8,7 +8,22 @@ const GetStarted = () => {
       rounded-full bg-blue-gradient
       p-[2px] cursor-pointer`}>
       <div
-        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}></div>
+        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%]
+        rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px]">
+            <span className="text-gradient">Get</span>
+          </p>
+          <img
+            src={arrowUp}
+            alt="getlink"
+            className="w-[23px] h-[23px] object-contain"
+          />
+        </div>
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">started</span>
+        </p>
+      </div>
     </div>
   );
 };
