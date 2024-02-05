eb1e4c4697cbee6af34756ce196fd6852d2b7df1
madjo <78071957+madjo7@users.noreply.github.com>
2024-02-05 18:48:40 +0100

    Added mid section and icons, button

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,11 +2,45 @@ import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
 
+const FeatureCard = ({ icon, title, content, index }) => {
+  return (
+    <div
+      className={`flex flex-row p-6 rounded-[20px] ${
+        index !== features.length - 1 ? "mb-6" : "mb-0"
+      } feature-card`}>
+      <div className={`w-[64px] h-[64px] rounded-full`}>
+        <img
+          src={icon}
+          alt="icon"
+          className="w-[50%] h-[50%] object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
 const Business = () => {
   return (
     <section
       id="features"
-      className={layout.section}></section>
+      className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we will handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px]`}>
+          With the right partner, you can improve your financial life by
+          building credit, earning rewards and saving money.
+        </p>
+        <Button styles="mt-10"></Button>
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feat, i) => (
+          <FeatureCard key={feat.id} />
+        ))}
+      </div>
+    </section>
   );
 };
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,13 @@
-import React from "react";
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-white 
+      font-poppins font-medium text-[18px]
+       text-primary outline-none ${styles}`}>
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
