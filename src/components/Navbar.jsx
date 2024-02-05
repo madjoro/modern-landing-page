a4b062eba40b051b0a03a8a1be10525b6ca42a00
madjo <78071957+madjo7@users.noreply.github.com>
2024-02-05 12:23:04 +0100

    Added navbar + mobile responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleNav, settoggleNav] = useState(false);
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"></img>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((link, i) => (
+          <li
+            key={link.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+              i === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggleNav ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => settoggleNav((prev) => !prev)}
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
